c20b41958aa081dfa91efbebe730399f0f32b3d6
geovaneSC <110263118+geovaneSC@users.noreply.github.com>
2024-06-09 00:16:42 -0300

    primeiro commit

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+function clicar(){
+    let menu = document.getElementById("navbarNav");
+    menu.classList.toggle("d-none");
+}
+function entrar(){
+    let menu = document.getElementById("navbarNav");
+    menu.style.display = "block"
+}
